80417ad5f8a7bc9a4575697edf4d267642d12024
Matheus Trindade <trindadematheus27@gmail.com>
2019-09-19 23:09:19 -0300

    Resolvendo bug de retorno nulo

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -3,7 +3,7 @@ const User = require('../app/models/user');
 module.exports = {
     Query: {
         users: () => { return User.findAll() },
-        user: (_, { id }) => { return User.findAll({ where: { id } }) },
+        user: (_, { id }) => { return User.findOne({ where: { id } }) },
     },
     Mutation: {
         createUser: (_, { name, email }) => { return User.create({ name, email }) }
